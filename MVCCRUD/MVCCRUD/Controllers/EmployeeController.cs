c95b4af479a1020b2da2485aa402c8fe115364af
tkmik <tkach_nikita@outlook.com>
2021-05-08 22:23:39 +0300

    added a new project to work with ASP.NET Core MVC using the Scaffold item

---
+++ MVCCRUD/MVCCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUD.Models;
+
+namespace MVCCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employee
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: Employee/Create
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                return View(await _context.Employees.FirstOrDefaultAsync(x => x.Id == id));
+            }
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("Id,FirstName,LastName,EmpCode,Position,OfficeLocation")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (employee.Id == 0)
+                {
+                    _context.Add(employee);
+                }
+                else
+                {
+                    _context.Update(employee);
+                }               
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(x=>x.Id == id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MVCCRUD/MVCCRUD/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required()]
+        [Column(TypeName = "NVARCHAR(50)")]
+        public string FirstName { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(50)")]
+        public string LastName { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(10)")]
+        [DisplayName("Emp. Code")]
+        public string EmpCode { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(100)")]
+        public string Position { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
