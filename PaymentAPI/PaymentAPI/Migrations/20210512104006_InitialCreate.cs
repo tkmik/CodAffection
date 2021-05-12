b4f7a43fa945440672b0a7164b8bb0ea84c718be
tkmik <tkach_nikita@outlook.com>
2021-05-12 13:48:46 +0300

    added a new project to work with Web Api to help with Scaffold item

---
+++ PaymentAPI/PaymentAPI/Migrations/20210512104006_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOrderName = table.Column<string>(type: "NVARCHAR(100)", nullable: false),
+                    CardNumber = table.Column<string>(type: "NVARCHAR(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "NVARCHAR(5)", nullable: false),
+                    SecurityCode = table.Column<string>(type: "NVARCHAR(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(100)")]
+        public string CardOrderName { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(16)")]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(5)")]
+        public string ExpirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "NVARCHAR(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
